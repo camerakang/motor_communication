fdff6001e8616063d8a06651d9b45bd4fa9ae67e
camerakang <81615179+camerakang@users.noreply.github.com>
2024-04-15 15:35:31 +0800

    阻塞发送

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 /*
-   RadioLib nRF24 Transmit with Interrupts Example
+   RadioLib nRF24 Transmit Example
 
    This example transmits packets using nRF24 2.4 GHz radio module.
    Each packet contains up to 32 bytes of data, in the form of:
@@ -18,17 +18,8 @@
 
 // include the library
 #include <RadioLib.h>
-#include <SPI.h>
-// MISO  6
-// MOSI  5
-// NSS:  3
-// CLK:  4
-void setFlag(void);
 
 // nRF24 has the following connections:
-// CS pin:    3
-// IRQ pin:   7
-// CE pin:    2
 #define MISO_24G 6
 #define MOSI_24G 5
 #define CS_24G 3
@@ -42,9 +33,6 @@ nRF24 radio = new Module(CS_24G, IRQ_24G, CE_24G, RADIOLIB_NC, radio_spi, spiSet
 // https://github.com/jgromes/RadioShield
 // nRF24 radio = RadioShield.ModuleA;
 
-// save transmission state between loops
-int transmissionState = RADIOLIB_ERR_NONE;
-
 void setup()
 {
   Serial.begin(115200);
@@ -52,7 +40,6 @@ void setup()
   // initialize nRF24 with default settings
   Serial.print(F("[nRF24] Initializing ... "));
   radio_spi.begin(SCK_24G, MISO_24G, MOSI_24G, CS_24G);
-radio_begin:
   int state = radio.begin();
   if (state == RADIOLIB_ERR_NONE)
   {
@@ -60,10 +47,10 @@ radio_begin:
   }
   else
   {
-
     Serial.print(F("failed, code "));
     Serial.println(state);
-    goto radio_begin;
+    while (true)
+      ;
   }
 
   // set transmit address
@@ -84,40 +71,7 @@ radio_begin:
     while (true)
       ;
   }
-
-  // set the function that will be called
-  // when packet transmission is finished
-  radio.setPacketSentAction(setFlag);
-
-  // start transmitting the first packet
-  Serial.print(F("[nRF24] Sending first packet ... "));
-
-  // you can transmit C-string or Arduino string up to
-  // 256 characters long
-  transmissionState = radio.startTransmit("Hello World!");
-
-  // you can also transmit byte array up to 256 bytes long
-  /*
-    byte byteArr[] = {0x01, 0x23, 0x45, 0x67,
-                      0x89, 0xAB, 0xCD, 0xEF};
-    state = radio.startTransmit(byteArr, 8);
-  */
-}
-
-// flag to indicate that a packet was sent
-volatile bool transmittedFlag = false;
-
-// this function is called when a complete packet
-// is transmitted by the module
-// IMPORTANT: this function MUST be 'void' type
-//            and MUST NOT have any arguments!
-#if defined(ESP8266) || defined(ESP32)
-ICACHE_RAM_ATTR
-#endif
-void setFlag(void)
-{
-  // we sent a packet, set the flag
-  transmittedFlag = true;
+  
 }
 
 // counter to keep track of transmitted packets
@@ -125,48 +79,41 @@ int count = 0;
 
 void loop()
 {
-  // check if the previous transmission finished
-  if (transmittedFlag)
+  Serial.print(F("[nRF24] Transmitting packet ... "));
+
+  // you can transmit C-string or Arduino string up to
+  // 32 characters long
+  String str = "Hello World! #" + String(count++);
+  int state = radio.transmit(str);
+
+  if (state == RADIOLIB_ERR_NONE)
   {
-    // reset flag
-    transmittedFlag = false;
-
-    if (transmissionState == RADIOLIB_ERR_NONE)
-    {
-      // packet was successfully sent
-      Serial.println(F("transmission finished!"));
-
-      // NOTE: when using interrupt-driven transmit method,
-      //       it is not possible to automatically measure
-      //       transmission data rate using getDataRate()
-    }
-    else
-    {
-      Serial.print(F("failed, code "));
-      Serial.println(transmissionState);
-    }
-
-    // clean up after transmission is finished
-    // this will ensure transmitter is disabled,
-    // RF switch is powered down etc.
-    radio.finishTransmit();
-
-    // wait a second before transmitting again
-    delay(1000);
-
-    // send another one
-    Serial.print(F("[nRF24] Sending another packet ... "));
-
-    // you can transmit C-string or Arduino string up to
-    // 32 characters long
-    String str = "Hello World! #" + String(count++);
-    transmissionState = radio.startTransmit(str);
-
-    // you can also transmit byte array up to 256 bytes long
-    /*
-      byte byteArr[] = {0x01, 0x23, 0x45, 0x67,
-                        0x89, 0xAB, 0xCD, 0xEF};
-      int state = radio.startTransmit(byteArr, 8);
-    */
+    // the packet was successfully transmitted
+    Serial.println(F("success!"));
   }
+  else if (state == RADIOLIB_ERR_PACKET_TOO_LONG)
+  {
+    // the supplied packet was longer than 32 bytes
+    Serial.println(F("too long!"));
+  }
+  else if (state == RADIOLIB_ERR_ACK_NOT_RECEIVED)
+  {
+    // acknowledge from destination module
+    // was not received within 15 retries
+    Serial.println(F("ACK not received!"));
+  }
+  else if (state == RADIOLIB_ERR_TX_TIMEOUT)
+  {
+    // timed out while transmitting
+    Serial.println(F("timeout!"));
+  }
+  else
+  {
+    // some other error occurred
+    Serial.print(F("failed, code "));
+    Serial.println(state);
+  }
+
+  // wait for a second before transmitting again
+  delay(1000);
 }
