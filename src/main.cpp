0d9da7fe7976feaaf4e67cd9af85e2b3e434554e
camerakang <81615179+camerakang@users.noreply.github.com>
2024-04-15 18:15:58 +0800

    中断接收实现

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@
 
 // include the library
 #include <RadioLib.h>
-
+void IRAM_ATTR setFlag(void);
 // nRF24 has the following connections:
 #define MISO_24G 6
 #define MOSI_24G 5
@@ -29,6 +29,8 @@
 SPIClass radio_spi(HSPI);
 SPISettings spiSettings(2000000, MSBFIRST, SPI_MODE0);
 nRF24 radio = new Module(CS_24G, IRQ_24G, CE_24G, RADIOLIB_NC, radio_spi, spiSettings);
+// nRF24 radio = new Module(3, 7, 2);
+
 // or using RadioShield
 // https://github.com/jgromes/RadioShield
 // nRF24 radio = RadioShield.ModuleA;
@@ -36,7 +38,7 @@ nRF24 radio = new Module(CS_24G, IRQ_24G, CE_24G, RADIOLIB_NC, radio_spi, spiSet
 void setup()
 {
   Serial.begin(115200);
-
+  pinMode(IRQ_24G, INPUT);
   // initialize nRF24 with default settings
   Serial.print(F("[nRF24] Initializing ... "));
   radio_spi.begin(SCK_24G, MISO_24G, MOSI_24G, CS_24G);
@@ -53,13 +55,13 @@ void setup()
       ;
   }
 
-  // set transmit address
+  // set receive pipe 0 address
   // NOTE: address width in bytes MUST be equal to the
   //       width set in begin() or setAddressWidth()
   //       methods (5 by default)
+  Serial.print(F("[nRF24] Setting address for receive pipe 0 ... "));
   byte addr[] = {0x01, 0x23, 0x45, 0x67, 0x89};
-  Serial.print(F("[nRF24] Setting transmit pipe ... "));
-  state = radio.setTransmitPipe(addr);
+  state = radio.setReceivePipe(0, addr);
   if (state == RADIOLIB_ERR_NONE)
   {
     Serial.println(F("success!"));
@@ -71,49 +73,94 @@ void setup()
     while (true)
       ;
   }
-  
-}
+  // set the function that will be called
+  // when new packet is received
+  radio.setPacketReceivedAction(setFlag);
 
-// counter to keep track of transmitted packets
-int count = 0;
-
-void loop()
-{
-  Serial.print(F("[nRF24] Transmitting packet ... "));
-
-  // you can transmit C-string or Arduino string up to
-  // 32 characters long
-  String str = "Hello World! #" + String(count++);
-  int state = radio.transmit(str);
-
+  // start listening
+  Serial.print(F("[nRF24] Starting to listen ... "));
+  state = radio.startReceive();
   if (state == RADIOLIB_ERR_NONE)
   {
-    // the packet was successfully transmitted
     Serial.println(F("success!"));
-  }
-  else if (state == RADIOLIB_ERR_PACKET_TOO_LONG)
-  {
-    // the supplied packet was longer than 32 bytes
-    Serial.println(F("too long!"));
-  }
-  else if (state == RADIOLIB_ERR_ACK_NOT_RECEIVED)
-  {
-    // acknowledge from destination module
-    // was not received within 15 retries
-    Serial.println(F("ACK not received!"));
-  }
-  else if (state == RADIOLIB_ERR_TX_TIMEOUT)
-  {
-    // timed out while transmitting
-    Serial.println(F("timeout!"));
   }
   else
   {
-    // some other error occurred
-    Serial.print(F("failed, code "));
-    Serial.println(state);
+
+    while (true)
+    {
+      Serial.print(F("failed, code "));
+      Serial.println(state);
+    };
   }
 
-  // wait for a second before transmitting again
-  delay(1000);
+  // if needed, 'listen' mode can be disabled by calling
+  // any of the following methods:
+  //
+  // radio.standby()
+  // radio.sleep()
+  // radio.transmit();
+  // radio.receive();
+  // radio.readData();
+}
+
+// flag to indicate that a packet was received
+volatile bool receivedFlag = false;
+
+// this function is called when a complete packet
+// is received by the module
+// IMPORTANT: this function MUST be 'void' type
+//            and MUST NOT have any arguments!
+#if defined(ESP8266) || defined(ESP32)
+ICACHE_RAM_ATTR
+#endif
+void  setFlag(void)
+{
+  // we got a packet, set the flag
+  receivedFlag = true;
+  String str1;
+  radio.readData(str1);
+}
+
+void loop()
+{
+  String str1;
+
+  // check if the flag is set
+
+  if (receivedFlag)
+  {
+    // reset flag
+    receivedFlag = false;
+
+    // you can read received data as an Arduino String
+    String str;
+    int state = radio.readData(str);
+
+    // you can also read received data as byte array
+    /*
+      byte byteArr[8];
+      int numBytes = radio.getPacketLength();
+      int state = radio.readData(byteArr, numBytes);
+    */
+
+    if (state == RADIOLIB_ERR_NONE)
+    {
+      // packet was successfully received
+      Serial.println(F("[nRF24] Received packet!"));
+
+      // print data of the packet
+      Serial.print(F("[nRF24] Data:\t\t"));
+      Serial.println(str);
+    }
+    else
+    {
+      // some other error occurred
+      Serial.print(F("[nRF24] Failed, code "));
+      Serial.println(state);
+    }
+
+    // put module back to listen mode
+    radio.startReceive();
+  }
 }
